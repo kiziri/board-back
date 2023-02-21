00eb010441cdfb494733fd015dd25302925a9b6b
kiziri <ekelekeleh@naver.com>
2023-02-21 09:36:38 +0900

    서비스 구조 세팅

---
+++ src/main/java/com/board/boardback/service/BoardService.java
@@ -0,0 +1,4 @@
+package com.board.boardback.service;
+
+public interface BoardService {
+}
---
+++ src/main/java/com/board/boardback/service/BoardServiceImpl.java
@@ -0,0 +1,4 @@
+package com.board.boardback.service;
+
+public class BoardServiceImpl implements BoardService {
+}
