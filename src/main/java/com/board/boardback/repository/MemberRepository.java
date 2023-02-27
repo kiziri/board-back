939bac53153e0a934d2ab0596fe847783422d91c
kiziri <ekelekeleh@naver.com>
2023-02-27 15:27:04 +0900

    DAO 설정

--- src/main/java/com/board/boardback/repository/MemberRepository.java
+++ src/main/java/com/board/boardback/repository/MemberRepository.java
@@ -1,8 +1,40 @@
 package com.board.boardback.repository;
 
+import com.board.boardback.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class MemberRepository {
+import javax.persistence.EntityManager;
+import java.util.List;
 
+@Repository
+@RequiredArgsConstructor
+public class MemberRepository {
+    private final EntityManager em;
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class).getResultList();
+    }
+
+    public Member findById(String memberId) {
+        return em.find(Member.class, memberId);
+    }
+
+    public void save(Member member) {       // 회원가입
+        em.persist(member);
+    }
+
+
+    public Member findByLoginInfo(String memberId, String memberPw) {
+        return em.createQuery("select m from Member m where m.memberId = :memberId " +
+                        "and m.memberPw = :memberPw", Member.class)
+                .setParameter("memberId", memberId)
+                .setParameter("memberPw", memberPw)
+                .getSingleResult();
+    }
+
+    public List<Member> findByEmailForDuplicate(String memberEmail) {
+        return em.createQuery("select m from Member m where m.memberId = :memberEmail", Member.class)
+                .setParameter("memberEmail", memberEmail).getResultList();
+    }
 }
