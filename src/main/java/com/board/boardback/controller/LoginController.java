a5f55a23e3e08c28c1e1eb1ba8f67a2969440cdb
kiziri <ekelekeleh@naver.com>
2023-03-06 00:26:43 +0900

    로그인 서비스 추가 및 수정

--- src/main/java/com/board/boardback/controller/LoginController.java
+++ src/main/java/com/board/boardback/controller/LoginController.java
@@ -18,20 +18,32 @@ import javax.servlet.http.HttpSession;
 public class LoginController {
     private final MemberServiceImpl memberService;
 
-    @PostMapping("/requestlogin")
+    @PostMapping("/requestLogin")
     public MemberDto getLoginMemberInfo(@ApiParam(value = "로그인 req", required = true)
-                                            @RequestBody MemberDto memberDto, HttpServletRequest request) {
+                                            @RequestBody MemberDto memberDto/*, HttpServletRequest request*/) {
 
 
         boolean isLogin = memberService.findByMemberIdAndMemberPw(memberDto);
         if (isLogin) {
             MemberDto loginMember = memberService.findById(memberDto);
-            HttpSession httpSession = request.getSession(true);
-            httpSession.setAttribute("member", loginMember);
-
+//            HttpSession httpSession = request.getSession(true);
+//            httpSession.setAttribute("member", loginMember);
             return loginMember;
         }
-
         return new MemberDto();
+    }
+
+    @PostMapping("/findPassword")
+    public MemberDto findMemberPassword(@ApiParam(value = "회원 비밀번호 찾기 req", required = true)
+                                @RequestBody MemberDto memberDto) {
+
+        MemberDto searchMember = memberService.findById(memberDto);
+
+        if (searchMember.getMemberId().equals(memberDto.getMemberId())) {
+            memberService.updateMember(memberDto);
+            searchMember = memberService.findById(memberDto);
+        }
+
+        return searchMember;
     }
 }
